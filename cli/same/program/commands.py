97a0dfe6a80ff306d2e40c37daad3c013a5593cc
David Aronchick <aronchick@gmail.com>
2021-09-02 17:10:49 +0000

    added loading and parsing the same.yaml file, flags for target and persisting compilation, and commented flags we will need (soon) from old code

--- cli/same/program/commands.py
+++ cli/same/program/commands.py
@@ -1,4 +1,7 @@
+from email.policy import default
+from pydoc import describe
 import click
+from ruamel.yaml import YAML  # Chose ruamel over pyyaml due to default yaml 1.2 support
 
 
 @click.group()
@@ -7,6 +10,37 @@ def program():
 
 
 @program.command()
-def compile():
+@click.option(
+    "-p",
+    "--persist-temp-files",
+    "persist_temp_files",
+    default=False,
+    is_flag=True,
+    type=bool,
+    help="Persist the temporary compilation files.",
+)
+@click.option(
+    "-f",
+    "--same-file",
+    "same_file",
+    type=click.File("rb"),
+    default="same.yaml",
+    help="Configuration file (same.yaml) for this project. We currently only support notebooks/python files describing pipelines in the same directory as the same configuration file.",
+    show_default=True,
+)
+@click.option(
+    "-t",
+    "--target",
+    type=click.Choice(["kubeflow", "aml"]),
+)
+def compile(same_file, persist_temp_files, target):
     """Compile a SAME program without running"""
-    click.echo("foobaz")
+    click.echo(f"File is: {same_file}")
+
+    yaml = YAML(typ="safe")
+    same_config = yaml.load(same_file)  # noqa: F841
+
+    # compileProgramCmd.Flags().String("image-pull-secret-server", "", "Image pull server for any private repos (only one server currently supported for all private repos)")
+    # compileProgramCmd.Flags().String("image-pull-secret-username", "", "Image pull username for any private repos (only one username currently supported for all private repos)")
+    # compileProgramCmd.Flags().String("image-pull-secret-password", "", "Image pull password for any private repos (only one password currently supported for all private repos)")
+    # compileProgramCmd.Flags().String("image-pull-secret-email", "", "Image pull email for any private repos (only one email currently supported for all private repos)")
--- test/cli/test_compile.py
+++ test/cli/test_compile.py
@@ -2,8 +2,8 @@ from click.testing import CliRunner
 from cli.same.program.commands import compile
 
 
-def test_compile():
-    # just testing that we can test.
+def test_compile_verb():
+    # just testing that we can test the compile verb
     runner = CliRunner()
     result = runner.invoke(compile)
     assert result.exit_code == 0
