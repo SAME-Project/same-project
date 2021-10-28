9c43b40ece61664f5390aee7aa1db36c8549b45e
Gohar Irfan Chaudhry <gochaudh@microsoft.com>
2021-10-28 11:23:34 -0700

    Adding .compute for sum operation

--- test/transformers/test_pandas_to_dask_transformer.py
+++ test/transformers/test_pandas_to_dask_transformer.py
@@ -23,3 +23,31 @@ print(df)
 
         assert expected_code == step.code
         assert ['dask.dataframe'] == step.packages_to_install
+
+    def test_sum(self):
+        test_csv_input_path = "testdata/table1.csv"
+
+        code = f"""import pandas as pd
+df = pd.read_csv('{test_csv_input_path}')
+x = df.sum()
+print(x)
+"""
+
+        step = Step(code=code)
+        transformer = PandasToDaskTransformer()
+        transformer.transform_step(step)
+
+        expected_code = f"""import dask.dataframe as dd
+import pandas as pd
+df = dd.read_csv('{test_csv_input_path}')
+x = df.sum().compute()
+print(x)
+"""
+
+        assert expected_code == step.code
+        assert ['dask.dataframe'] == step.packages_to_install
+
+    def test_no_translation_sum(self):
+        # x = [1,2,3]
+        # y = sum(x)
+        pass
--- transformers/pandas_to_dask_transformer.py
+++ transformers/pandas_to_dask_transformer.py
@@ -15,6 +15,10 @@ class PandasToDaskTransformer(ast.NodeTransformer, Transformer):
         self._pandas_import_asname = 'pd'
         # Supported Pandas functions that can be 1:1 mapped to Dask
         self._pandas_to_dask_functions = ['read_csv']
+        # Pandas functions for which we need to call .compute when translated to Dask
+        self._pandas_to_dask_with_compute_functions = ['sum']
+        # Function call for .compute
+        self._dask_compute = 'compute'
 
     def _import_dask_dataframe(self):
         """
@@ -23,21 +27,28 @@ class PandasToDaskTransformer(ast.NodeTransformer, Transformer):
         if self._dask_dataframe_import_name not in self._required_imports:
             self._required_imports[self._dask_dataframe_import_name] = self._dask_dataframe_import_asname
 
-    def _is_translation_valid(self, attr, attr_id):
+    def _is_one_to_one_mapping_valid(self, node):
         """
         Checks if it is valid to convert the given function call from Pandas to Dask.
         """
+        attr = node.func.attr
+        attr_id = node.func.value.id
         if attr not in self._pandas_to_dask_functions:
             return False
         if attr_id != self._pandas_import_name and attr_id != self._pandas_import_asname:
             return False
         return True
 
-    def visit_Call(self, node: ast.Call):
+    def _is_compute_required(self, node):
+        attr = node.func.attr
+        if attr in self._pandas_to_dask_with_compute_functions:
+            return True
+        return False
+
+    def _try_translate(self, node):
+        updated_node = None
         if isinstance(node.func, ast.Attribute):
-            attr = node.func.attr
-            attr_id = node.func.value.id
-            if self._is_translation_valid(attr, attr_id):
+            if self._is_one_to_one_mapping_valid(node):
                 self._import_dask_dataframe()
                 updated_node = ast.Call(
                     func=ast.Attribute(
@@ -51,7 +62,35 @@ class PandasToDaskTransformer(ast.NodeTransformer, Transformer):
                     args=node.args,
                     keywords=node.keywords
                 )
-                return updated_node
+            elif self._is_compute_required(node):
+                updated_node = ast.Call(
+                    func=ast.Attribute(
+                        value=ast.Call(
+                            func=ast.Attribute(
+                                value=ast.Name(
+                                    id=node.func.value.id,
+                                    ctx=node.func.ctx
+                                ),
+                                attr=node.func.attr,
+                                ctx=node.func.ctx
+                            ),
+                            args=node.args,
+                            keywords=node.keywords
+                        ),
+                        attr=self._dask_compute,
+                        ctx=node.func.ctx
+                    ),
+                    args=[],
+                    keywords=[]
+                )
+        return updated_node
+
+    def visit_Call(self, node: ast.Call):
+        translated_node = self._try_translate(node)
+        if translated_node is not None:
+            # Translation resulted in updating the node, so replace it
+            return translated_node
+        # No translation happened, keep the original node
         return node
 
     def transform_step(self, step: Step) -> None:
