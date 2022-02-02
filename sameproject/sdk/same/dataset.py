f8ace5eceee070b91a024ef8267e9ec3f1154596
js-ts <vedantpadwalinfi@gmail.com>
2022-02-03 04:12:28 +0530

    Copied David's Code from https://github.com/js-ts/fix-same-dataset-tests
    David Fixed Everything wrong

--- sameproject/sdk/same/dataset.py
+++ sameproject/sdk/same/dataset.py
@@ -1,51 +1,60 @@
+from multiprocessing.sharedctypes import Value
 import yaml
 import os
 from urllib.parse import urlparse
 import pandas as pd
+from sameproject.same_config import SameConfig
 
-def dataset(name, same_file = 'same.yaml'):
+
+def dataset(name, same_file="same.yaml"):
     """Imports the dataset based upon the env provided
        wrapper around pd.read_<file> and I/O APIs
     NOTE: currently tested for json,csv and ipfs
     """
     try:
-      env_var = os.environ['SAME_ENV'] if os.environ['SAME_ENV'] != "" else "default"
+        env_var = os.environ["SAME_ENV"] if os.environ["SAME_ENV"] != "" else "default"
     except:
-      env_var = "default"
-    with open(same_file, 'r') as file:
-        same_variables = yaml.safe_load(file)
+        env_var = "default"
+    with open(same_file, "rb+") as file:
+        same_config = SameConfig(file)
 
-    parsed_url = urlparse(same_variables['datasets'][name][env_var])
+    if name not in same_config.datasets:
+        raise ValueError(f"'{name}' is not a dataset in the same file at '{same_file}'.")
+
+    if env_var not in same_config.datasets[name].environments:
+        raise ValueError(f"'{env_var}' is not an environment in the '{name}' dataset in the same file at '{same_file}'.")
+
+    parsed_url = urlparse(same_config.datasets[name].environments[env_var])
     # If URL is an IPFS url, access it via the IPFS gateway
     if parsed_url.scheme == "ipfs":
-      url = 'https://gateway.ipfs.io/ipfs/'+parsed_url.netloc+parsed_url.path
+        url = "https://gateway.ipfs.io/ipfs/" + parsed_url.netloc + parsed_url.path
     else:
-      url = same_variables['datasets'][name][env_var]
+        url = same_config.datasets[name].environments[env_var]
     filename, file_extension = os.path.splitext(url)
-    
+
     extensions = {
-      '.csv': 'read_csv',
-      '.dta': 'read_stata',
-      '.feather': 'read_feather',
-      '.html': 'read_html',
-      '.json': 'read_json',
-      '.orc': 'read_orc',
-      '.parquet': 'read_parquet',
-      '.pickle': 'read_pickle',
-      '.sas': 'read_sas',
-      '.sav': 'read_spss',
-      '.sql': 'read_sql',
-      '.txt': 'read_fwf',
-      '.xml': 'read_xml',
-      ('.hdf', '.h4', '.hdf4', '.he2', '.h5', '.hdf5', '.he5'): 'read_hdf',
-      ('.xlsx', '.ods'): 'read_excel',
+        ".csv": "read_csv",
+        ".dta": "read_stata",
+        ".feather": "read_feather",
+        ".html": "read_html",
+        ".json": "read_json",
+        ".orc": "read_orc",
+        ".parquet": "read_parquet",
+        ".pickle": "read_pickle",
+        ".sas": "read_sas",
+        ".sav": "read_spss",
+        ".sql": "read_sql",
+        ".txt": "read_fwf",
+        ".xml": "read_xml",
+        (".hdf", ".h4", ".hdf4", ".he2", ".h5", ".hdf5", ".he5"): "read_hdf",
+        (".xlsx", ".ods"): "read_excel",
     }
-    
+
     for key, value in list(extensions.items()):
-      if type(key) == tuple:
-        for ext in key:
-          extensions[ext] = value
-        extensions.pop(key)
+        if type(key) == tuple:
+            for ext in key:
+                extensions[ext] = value
+            extensions.pop(key)
     reader = getattr(pd, extensions[file_extension])
     ds = reader(url)
     return ds
--- test/sdk/test_dataset.py
+++ test/sdk/test_dataset.py
@@ -2,24 +2,20 @@
 # pylint: disable=missing-module-docstring, missing-class-docstring
 # -*- coding: utf-8 -*-
 import sys
+
 print(sys.path)
 from pathlib import Path
+
 sys.path.append(Path(__file__).parent.parent.absolute().name)
 import sameproject.sdk.same as same
 from sameproject.same_config import SameConfig
-# import subprocess
-# import sys
-# import csv
 import os
 import pytest
-# import pytest_virtualenv as venv
-# from urllib.parse import urlparse
-# import yaml
-# import importlib
+from pytest_mock import mocker
+import pandas
 
-
-csv={
-    'longitude': [
+csv = {
+    "longitude": [
         -114.31,
         -114.47,
         -114.56,
@@ -30,8 +26,8 @@ csv={
         -114.59,
         -114.59,
         -114.6,
-        ],
-    'latitude': [
+    ],
+    "latitude": [
         34.19,
         34.4,
         33.69,
@@ -42,8 +38,8 @@ csv={
         34.83,
         33.61,
         34.83,
-        ],
-    'housing_median_age': [
+    ],
+    "housing_median_age": [
         15,
         19,
         17,
@@ -54,8 +50,8 @@ csv={
         41,
         34,
         46,
-        ],
-    'total_rooms': [
+    ],
+    "total_rooms": [
         5612,
         7650,
         720,
@@ -66,8 +62,8 @@ csv={
         812,
         4789,
         1497,
-        ],
-    'total_bedrooms': [
+    ],
+    "total_bedrooms": [
         1283,
         1901,
         174,
@@ -78,8 +74,8 @@ csv={
         168,
         1175,
         309,
-        ],
-    'population': [
+    ],
+    "population": [
         1015,
         1129,
         333,
@@ -90,8 +86,8 @@ csv={
         375,
         3134,
         787,
-        ],
-    'households': [
+    ],
+    "households": [
         472,
         463,
         117,
@@ -102,8 +98,8 @@ csv={
         158,
         1056,
         271,
-        ],
-    'median_income': [
+    ],
+    "median_income": [
         1.4936,
         1.82,
         1.6509,
@@ -114,8 +110,8 @@ csv={
         1.7083,
         2.1782,
         2.1908,
-        ],
-    'median_house_value': [
+    ],
+    "median_house_value": [
         66900,
         80100,
         85700,
@@ -126,42 +122,63 @@ csv={
         48500,
         58400,
         48100,
-        ],
-    }
-json={
-	"owl" : [
-	    {"item" : "<span class='item'><h1>1</h1></span>"},
-	    {"item" : "<span class='item'><h1>2</h1></span>"},
-	    {"item" : "<span class='item'><h1>3</h1></span>"},
-	    {"item" : "<span class='item'><h1>4</h1></span>"},
-	    {"item" : "<span class='item'><h1>5</h1></span>"},
-	    {"item" : "<span class='item'><h1>6</h1></span>"},
-	    {"item" : "<span class='item'><h1>7</h1></span>"},
-	    {"item" : "<span class='item'><h1>8</h1></span>"},
-	    {"item" : "<span class='item'><h1>9</h1></span>"},
-	    {"item" : "<span class='item'><h1>10</h1></span>"},
-	    {"item" : "<span class='item'><h1>11</h1></span>"},
-	    {"item" : "<span class='item'><h1>12</h1></span>"},
-	    {"item" : "<span class='item'><h1>13</h1></span>"},
-	    {"item" : "<span class='item'><h1>14</h1></span>"}
-	]
+    ],
+}
+json = {
+    "owl": [
+        {"item": "<span class='item'><h1>1</h1></span>"},
+        {"item": "<span class='item'><h1>2</h1></span>"},
+        {"item": "<span class='item'><h1>3</h1></span>"},
+        {"item": "<span class='item'><h1>4</h1></span>"},
+        {"item": "<span class='item'><h1>5</h1></span>"},
+        {"item": "<span class='item'><h1>6</h1></span>"},
+        {"item": "<span class='item'><h1>7</h1></span>"},
+        {"item": "<span class='item'><h1>8</h1></span>"},
+        {"item": "<span class='item'><h1>9</h1></span>"},
+        {"item": "<span class='item'><h1>10</h1></span>"},
+        {"item": "<span class='item'><h1>11</h1></span>"},
+        {"item": "<span class='item'><h1>12</h1></span>"},
+        {"item": "<span class='item'><h1>13</h1></span>"},
+        {"item": "<span class='item'><h1>14</h1></span>"},
+    ]
 }
 
-@pytest.fixture
-def same_config():
-    with open('test/sdk/same.yaml', "rb") as f:
-        return SameConfig(buffered_reader=f)
+test_same_file_location = "test/sdk/same.yaml"
 
 
+simple_dataframe = pandas.DataFrame.from_dict({"A": [1]})
 
-def test_dataset_csv(same_config):
+
+def test_dataset_csv(mocker):
+    mocker.patch("pandas.read_csv", return_value=simple_dataframe)
+
     os.environ["SAME_ENV"] = ""
-    print(same.dataset(name="USER_HISTORY"))
-    assert list(csv) == list(same.dataset(name="USER_HISTORY"))
+    return_df = same.dataset(name="USER_HISTORY", same_file=test_same_file_location)
+    assert isinstance(return_df, pandas.DataFrame)
+    assert return_df.iloc[0]["A"] == 1
 
 
-def test_dataset_ipfs(same_config):
-    
-    os.environ["SAME_ENV"] = "IPFS"
-    print(same.dataset(name="USER_HISTORY"))
-    assert list(json) == list(same.dataset(name="USER_HISTORY"))
+# Tests to see if the system patches the URL with IPFS
+def test_dataset_ipfs(mocker):
+    mocker.patch("pandas.read_json", return_value=simple_dataframe)
+
+    os.environ["SAME_ENV"] = "prod-edge"
+    return_df = same.dataset(name="USER_HISTORY", same_file=test_same_file_location)
+    assert isinstance(return_df, pandas.DataFrame)
+    assert return_df.iloc[0]["A"] == 1
+
+
+def test_dataset_bad_environment():
+    os.environ["SAME_ENV"] = "BAD_ENVIRONMENT"
+    with pytest.raises(ValueError) as exception_info:
+        same.dataset(name="USER_HISTORY", same_file=test_same_file_location)
+
+    assert "BAD_ENVIRONMENT" in str(exception_info.value)
+
+
+def test_dataset_bad_dataset():
+    os.environ["SAME_ENV"] = "default"
+    with pytest.raises(ValueError) as exception_info:
+        same.dataset(name="BAD_DATASET", same_file=test_same_file_location)
+
+    assert "BAD_DATASET" in str(exception_info.value)
