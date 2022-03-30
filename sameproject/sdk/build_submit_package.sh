e89764d3f5d4ba277b520a55e34e5d97ef95b984
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-03-30 10:50:36 +0200

    Release sameproject 0.1.15, start cleaning up legacy packages

--- sameproject/sdk/build_submit_package.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-python3 setup.py sdist bdist_wheel
-python3 -m pip install --upgrade twine
-python3 -m twine upload dist/*
--- sameproject/sdk/same/__init__.py
+++
@@ -1,6 +0,0 @@
-from .importer import import_packages
-from .dataset import dataset
-from .conda_env import CondaEnv,CondaEnvValidator
-import sys
-
-sys.path.append("../../vendor")
