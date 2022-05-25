c7ca8391383d6a9cc912eaf171a9216eadfa0cd4
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-05-25 12:35:21 +0200

    Change deployment to use linux containers as windows doesn't support python runtime on azure.

--- sameproject/ops/durable_functions/setup.py
+++ sameproject/ops/durable_functions/setup.py
@@ -45,46 +45,37 @@ def provision_orchestrator(
         }
     )
     sa.wait()
-    print(sa.result())
 
     sak = sm_client.storage_accounts.list_keys(
         "same-resource-group",
         "samestorageaccount",
     )
     storage_string = f"DefaultEndpointsProtocol=https;AccountName=samestorageaccount;EndpointSuffix=core.windows.net;AccountKey={sak.keys[0].value}"
-    print(storage_string)
 
     asp = wm_client.app_service_plans.begin_create_or_update(
         "same-resource-group",
         "same-app-service-plan",
         {
-            "kind": "functionapp",
+            "kind": "linux",
+            "reserved": "true",
             "location": "West US",
-            "reserved": False,
             "sku": {
                 "name": "Y1",
-                "tier": "Dynamic",
-                "size": "Y1",
-                "family": "Y",
-                "capacity": 0
             },
         }
     )
     asp.wait()
-    print("HERE")
-    print(asp.result())
 
     wa = wm_client.web_apps.begin_create_or_update(
         "same-resource-group",
         "same-site",
         {
-            'location': "West US",
-            'server_farm_id': asp.result().id,
-            'reserved': False,
-            'enabled': True,
-            'kind': 'functionapp',
-            'site_config': {
-                "python_version": "3.9",
+            "kind": "functionapp,linux",
+            "location": "West US",
+            "reserved": True,
+            "server_farm_id": asp.result().id,
+            "site_config": {
+                "linux_fx_version": "PYTHON|3.9",
                 "app_settings": [
                     {
                         "name": "AzureWebJobsStorage",
@@ -103,14 +94,3 @@ def provision_orchestrator(
         }
     )
     wa.wait()
-    print("HERE")
-    print(wa.result())
-
-    # So we can actually hit a host name like this!
-
-    root_path = Path(__file__).parent / "orchestrator"
-    config_path = root_path / "function.json"
-    script_path = root_path / "__init__.py"
-    print(config_path.as_uri())
-    with config_path.open("r") as file:
-        config = json.loads(file.read())
