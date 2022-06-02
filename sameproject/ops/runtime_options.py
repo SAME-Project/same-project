2139fb61fcd623c2c38ec017abfa4cee48eb69de
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-06-02 20:20:37 +0200

    Move runtime options into backend packages and add support for custom validation. (#157)
    * Expand runtime_options module to support cerberus schemas and custom validation.
    
    * Move runtime options into each backend's package.
    
    * Fix test action.

--- sameproject/ops/runtime_options.py
+++ sameproject/ops/runtime_options.py
@@ -1,3 +1,5 @@
+from typing import Any, Callable, List, Optional
+from cerberus import Validator
 from box import Box
 import click
 import os
@@ -9,7 +11,7 @@ import os
 _registry = {}
 
 
-def runtime_options(fn):
+def runtime_options(fn) -> Callable:
     """Returns a click decorator composing all registered runtime options."""
     return _compose_decorators(
         fn,
@@ -17,7 +19,7 @@ def runtime_options(fn):
     )
 
 
-def runtime_schema():
+def runtime_schema() -> dict:
     """Returns a cerberus schema for validating runtime options."""
     schema = {
         "type": "dict",
@@ -25,22 +27,26 @@ def runtime_schema():
     }
 
     for opt in list_options():
-        schema["schema"][opt] = {
-            "type": _get_cerberus_type(
-                _registry[opt].name,
-                _registry[opt].type
-            )
-        }
+        opt_schema = _registry[opt].schema
+        if opt_schema is None:
+            opt_schema = {
+                "nullable": True,
+                "type": _get_cerberus_type(
+                    _registry[opt].name,
+                    _registry[opt].type
+                )
+            }
+        schema["schema"][opt] = opt_schema
 
     return schema
 
 
-def list_options():
+def list_options() -> List[str]:
     """Returns the list of registered options."""
     return _registry.keys()
 
 
-def get_option_value(name):
+def get_option_value(name: str) -> Any:
     """Returns the current value of the option with the given name."""
     if name not in _registry:
         raise Exception(f"Option registry does not contain name '{name}'.")
@@ -48,7 +54,7 @@ def get_option_value(name):
     return _registry[name].value
 
 
-def get_option_decorator(name):
+def get_option_decorator(name: str) -> Callable:
     """Returns a click decorator for setting an option via the command-line."""
     if name not in _registry:
         raise Exception(f"Option registry does not contain name '{name}'.")
@@ -61,21 +67,57 @@ def get_option_decorator(name):
         callback=_registry[name].callback,
         is_flag=_registry[name].type == bool,
         expose_value=False,  # don't affect click method signatures
-        required=False,  # TODO: support required runtime options?
         is_eager=True,  # handle runtime options before other options
     )
 
 
-def _compose_decorators(fn, decorators):
-    for decorator in decorators:
-        fn = decorator(fn)
-    return fn
-
-
-def _register_option(name: str, desc: str, type=str, flag=None, env=None):
+def validate_options(backend: str):
     """
-    Registers an option with the given internal name, command-line flag and
-    environment variable.
+    Raises if the runtime options are set incorrectly for the given backend.
+    Doing it with 'validator' functions allows us to have options that are
+    conditionally dependent on each other for a particular backend.
+    """
+    opts = {}
+    for name in list_options():
+        opts[name] = get_option_value(name)
+
+    validator = Validator({"values": runtime_schema()})
+    if not validator.validate({"values": opts}):
+        raise SyntaxError(f"One or more runtime options is invalid: {validator.errors}")
+
+    for name in list_options():
+        if _registry[name].validator is not None:
+            _registry[name].validator(backend, name, opts)
+
+
+def required_for_backend(backend: str) -> Callable:
+    """Validator that marks an option as required for a given backend."""
+    def _inner(_backend, name, opts):
+        if _backend == backend:
+            if opts[name] is None:
+                raise Exception(f"Option '{name}' must be set for backend '{backend}'.")
+    return _inner
+
+
+def register_option(
+    name: str,
+    desc: str,
+    type: Any = str,
+    flag: Optional[str] = None,
+    env: Optional[str] = None,
+    schema: Optional[dict] = None,
+    validator: Callable[[str, str, dict], None] = None,
+):
+    """
+    Registers a runtime option with the given metadata.
+
+    :param name: The internal name used to refer to the option.
+    :param desc: A description of what the option is configuring.
+    :param type: The Python type of the option, e.g. 'str' or 'bool'.
+    :param flag: Command line flag used to set the option. Defaults to a kebab-case translation of 'name', e.g. 'my_opt' ==> '--my-opt'.
+    :param env: Environment variable used to set the option. Defaults to an upper-case translation of 'name', e.g. 'my_opt' ==> 'MY_OPT'.
+    :param schema: Cerberus schema used to validate the option once it's been set. Defaults to checking that the input matches 'type'.
+    :param validator: Function that receives the current backend, option name and bag of options and raises if the option has been set incorrectly.
     """
     if flag is None:
         flag = "--" + name.replace("_", "-")
@@ -94,11 +136,13 @@ def _register_option(name: str, desc: str, type=str, flag=None, env=None):
         "env": env,
         "type": type,
         "value": value,
+        "schema": schema,
+        "validator": validator,
         "callback": lambda ctx, param, value: setattr(_registry[name], "value", value),
     })
 
 
-def _get_cerberus_type(name, type):
+def _get_cerberus_type(name: str, type: Any) -> str:
     if type == str:
         return "string"
 
@@ -111,56 +155,20 @@ def _get_cerberus_type(name, type):
     raise TypeError(f"Runtime option '{name}' has unsupported type '{type}'.")
 
 
+def _compose_decorators(fn: Callable, decorators: List[Callable]) -> Callable:
+    for decorator in decorators:
+        fn = decorator(fn)
+    return fn
+
+
 # General SAME configuration:
-_register_option(
+register_option(
     "serialisation_memory_limit",
     "Maximum size in bytes allowed for variables being serialised between steps.",
     type=int,
 )
-_register_option(
+
+register_option(
     "same_env",
     "Environment to compile and deploy notebooks against.",
 )
-
-# Options for Kubeflow backend:
-_register_option(
-    "image_pull_secret_name",
-    "The name of the kubernetes secret to create for docker secrets.",
-)
-_register_option(
-    "image_pull_secret_registry_uri",
-    "URI of private docker registry for private image pulls.",
-)
-_register_option(
-    "image_pull_secret_username",
-    "Username for private docker registry for private image pulls.",
-)
-_register_option(
-    "image_pull_secret_password",
-    "Password for private docker registry for private image pulls.",
-)
-_register_option(
-    "image_pull_secret_email",
-    "Email address for private docker registry for private image pulls.",
-)
-
-# Options for Azure durable functions backend:
-_register_option(
-    "functions_subscription_id",
-    "Azure subscription ID in which to provision backend functions.",
-)
-_register_option(
-    "functions_skip_provisioning",
-    "Skip provisioning of azure functions resources, to be used only if they already exist.",
-    type=bool,
-)
-
-# Options for AML backend:
-# TODO: write help lines for each of these options
-_register_option("aml_compute_name", "")
-_register_option("aml_sp_password_value", "")
-_register_option("aml_sp_tenant_id", "")
-_register_option("aml_sp_app_id", "")
-_register_option("workspace_subscription_id", "")
-_register_option("workspace_resource_group", "")
-_register_option("workspace_name", "")
