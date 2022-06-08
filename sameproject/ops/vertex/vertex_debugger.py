482d4d44a05b3413f53fb65698081855a92e74eb
David Aronchick <aronchick@gmail.com>
2022-06-08 00:10:45 +0000

    updating vertex debugger

--- sameproject/ops/vertex/vertex_debugger.py
+++ sameproject/ops/vertex/vertex_debugger.py
@@ -1,4 +1,5 @@
 import sys
+import uuid
 import click
 import glob
 from pathlib import Path
@@ -84,36 +85,37 @@ def compile_vertex(compiled_directory: os.strerror):
     required=True,
 )
 @click.option(
-    "--service-account",
-    "service_account",
-    help="Service principal to run this pipeline under.",
-    show_default=True,
-    required=True,
-)
-@click.option(
     "--service-account-credentials-file",
     "service_account_credentials_file",
     help="JSON file containing credentials for the service account.",
     show_default=True,
     required=True,
 )
-def deploy_vertex(compiled_pipeline_path: str, project_id: str, service_account: str, service_account_credentials_file: str):
+def deploy_vertex(compiled_pipeline_path: str, project_id: str, service_account_credentials_file: str):
     dotenv.load_dotenv()
     from google.cloud import aiplatform
 
     project_id = os.environ.get("PROJECT_ID", project_id)
-    service_account = os.environ.get("SERVICE_ACCOUNT_ID", service_account)
     service_account_credentials_file = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", service_account_credentials_file)
-    location = "northamerica-northeast2"
+    location = "northamerica-northeast1"
 
-    aiplatform.init(project=project_id, location=location, credentials=service_account_credentials_file)
+    from google.oauth2 import service_account
+
+    credentials = service_account.Credentials.from_service_account_file(service_account_credentials_file)
+
+    # aiplatform.init(project=project_id, location=location, credentials=credentials)
+
+    BUCKET_NAME = os.environ.get("BUCKET_NAME")
+    BUCKET_URI = f"gs://{BUCKET_NAME}"
+    PIPELINE_ROOT = f"{BUCKET_URI}/{uuid.uuid4()}/pipeline_root"
 
     job = aiplatform.PipelineJob(
         display_name="MY_DISPLAY_JOB",
         template_path=compiled_pipeline_path,
         project=project_id,
-        credentials=service_account_credentials_file,
+        credentials=credentials,
         location=location,
+        pipeline_root=f"{PIPELINE_ROOT}",
     )
 
     # job = aiplatform.PipelineJob(
@@ -130,7 +132,7 @@ def deploy_vertex(compiled_pipeline_path: str, project_id: str, service_account:
     #     location=LOCATION,
     # )
 
-    job.submit()
+    job.submit(service_account=credentials.service_account_email)
     # job.submit(service_account=SERVICE_ACCOUNT, network=NETWORK)
 
 
