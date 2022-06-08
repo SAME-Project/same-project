c250b5bab561668f092fce4c397dd381f0efd25d
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-06-08 09:45:18 +0200

    Harden 'same init' against bugs found by fuzzer. (#159)

--- test/cli/fuzzer.py
+++ test/cli/fuzzer.py
@@ -2,31 +2,37 @@ from random import random, sample
 from typing import Mapping
 import pytest
 
-example_params = {
-    "abcdeABCDE": 16,
+# A simple parameter mapping that mostly generates alphanums, but occasionally
+# includes some special characters of different kinds.
+std = {
+    "abcdeABCDE1234": 16,
     "_-": 8,
-    "'`\"": 2,
+    "\\/": 2,
+    "@#$%^&": 2,
+    "'`\"": 1,
     "(){}<>": 1,
-    "@#$%^&": 1,
+}
+
+# A parameter mapping for generating file names.
+file = {
+    "abcdeABCDE1234": 16,
+    "_-": 8,
+    "@#$%^&": 2,
+    "(){}<>": 1,
 }
 
 
-# An example of how to use genstr(...):
-def test_genstr():
-    for _ in range(10):
-        print(genstr(example_params))
-
-
-def genstr(params: Mapping[str, int], dp: float = 0.05) -> str:
+def genstr(params: Mapping[str, int], dp: float = 0.05, minlen: int = 0) -> str:
     """
     Generates a random string where each char is generated with probability
     corresponding to its weight in the `params` mapping.
         :param params: maps strings to their char's weights
         :param p:      tunes the average length of the results
+        :param minlen: minimum length of a string to generate
     """
     p = 0.1
     res = ""
-    while random() > p:
+    while len(res) < minlen or random() > p:
         res += _genchar(params)
         p += dp
 
