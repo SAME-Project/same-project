85c837b94b98505e8f7f12f7ca6789375a148284
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-06-08 10:44:04 +0200

    Use cerberus schema for runtime option validation and filter based on backend.

--- sameproject/ops/aml/options.py
+++ sameproject/ops/aml/options.py
@@ -1,9 +1,9 @@
-from sameproject.ops.runtime_options import register_option, required_for_backend
+from sameproject.ops.runtime_options import register_option
 
 register_option(
     "aml_compute_name",
     "Azure compute name for the pre-provisioned compute cluster.",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -16,7 +16,7 @@ register_option(
 register_option(
     "aml_sp_password_value",
     "Password for the service principal necessary for running this AML job. Read more here: https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -26,7 +26,7 @@ register_option(
 register_option(
     "aml_sp_tenant_id",
     "Tenant for the service principal necessary for running this AML job. Read more here: https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -37,7 +37,7 @@ register_option(
 register_option(
     "aml_sp_app_id",
     "Service Principal App ID for the service principal necessary for running this AML job. Read more here: https://docs.microsoft.com/en-us/cli/azure/create-an-azure-service-principal-azure-cli",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -48,7 +48,7 @@ register_option(
 register_option(
     "workspace_subscription_id",
     "Workspace ID for your AML backend. Read more here: https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-environment#workspace",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -59,7 +59,7 @@ register_option(
 register_option(
     "workspace_resource_group",
     "Workspace resource group for your AML backend. Read more here: https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-environment#workspace",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
@@ -70,7 +70,7 @@ register_option(
 register_option(
     "workspace_name",
     "Workspace name for your AML backend. Read more here: https://docs.microsoft.com/en-us/azure/machine-learning/how-to-configure-environment#workspace",
-    validator=required_for_backend("aml"),
+    backend="aml",
     schema={
         "nullable": False,
         "type": "string",
