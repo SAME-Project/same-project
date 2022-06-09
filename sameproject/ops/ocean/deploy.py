6f31c14bbbc8f211df0c054e71ef3f2aefd863ee
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-09 13:44:32 +0200

    WIP: init render & deploy methods

---
+++ sameproject/ops/ocean/deploy.py
@@ -0,0 +1,5 @@
+from sameproject.data.config import SameConfig
+from pathlib import Path
+
+def deploy(base_path: Path, root_file: str, config: SameConfig):
+    return
---
+++ sameproject/ops/ocean/render.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+from typing import Tuple
+
+def render(compile_path: str, steps: list, same_config: dict) -> Tuple[Path, str]:
+    return
