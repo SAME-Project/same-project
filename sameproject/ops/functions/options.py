bf1926ccdb9397b34358670537ee0f1c70d0f2f0
Guy Paterson-Jones <guy.paterson.jones@gmail.com>
2022-06-15 16:45:06 +0200

    Get local dev working for functions backend, fix up docs and remove unnecessary options.

--- sameproject/ops/functions/options.py
+++ sameproject/ops/functions/options.py
@@ -1,20 +1,21 @@
 from sameproject.ops.runtime_options import register_option
 
 register_option(
-    "functions_subscription_id",
-    "Azure subscription ID containing the deployed 'functions' backend.",
-    backend="functions",
-    schema={
-        "type": "string",
-        "regex": r"^[\d\w-]+",
-    },
-)
-
-register_option(
     "functions_host_name",
     "Hostname of the deployed 'functions' backend.",
     backend="functions",
     schema={
         "type": "string",
+    },
+)
+
+register_option(
+    "functions_use_http",
+    "Use http instead of https for communicating with 'functions' backend.",
+    type=bool,
+    backend="functions",
+    default_value=False,
+    schema={
+        "type": "boolean",
     },
 )
