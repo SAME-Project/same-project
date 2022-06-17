d2a1e10a01e4a04913601f4aec23d0f6001f1581
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-17 08:36:35 +0100

    WIP: working printing deploy

--- sameproject/ops/ocean/deploy.py
+++ sameproject/ops/ocean/deploy.py
@@ -1,5 +1,11 @@
 from sameproject.data.config import SameConfig
+from sameproject.ops import helpers
 from pathlib import Path
+import importlib
+
 
 def deploy(base_path: Path, root_file: str, config: SameConfig):
+    with helpers.add_path(str(base_path)):
+        root_module = importlib.import_module(root_file)  # python module
+        print(f"Root module is {root_module}")
     return
