345707345d6eee0548cf7ebadd5602e5112f373d
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-30 09:37:27 +0100

    FIX: working, modular deploy, passed test

--- sameproject/ops/ocean/deploy.py
+++ sameproject/ops/ocean/deploy.py
@@ -20,6 +20,44 @@ def deploy(base_path: Path,
            root_file: str, # root function with notebook code (string)
            config: SameConfig):
 
+    ocean, OCEAN_token, provider_url = configure_ocean(config=config)
+    wallet = Wallet(ocean.web3, config.runtime_options.get("wallet_private_key"), transaction_timeout=20, block_confirmations=0)
+    print(f"wallet.address = '{wallet.address}'")
+    assert wallet.web3.eth.get_balance(wallet.address) > 0, "need ETH"
+
+    ALG_ddo, ALG_datatoken = algo_publish(config, wallet, ocean, provider_url)
+    
+    DATA_did = config.runtime_options.get("dt_did")
+    ALG_did = ALG_ddo.did
+    DATA_DDO = ocean.assets.resolve(DATA_did)  # make sure we operate on the updated and indexed metadata_cache_uri versions
+    ALG_DDO = ocean.assets.resolve(ALG_did)
+    while ALG_DDO == None:
+        ALG_DDO = ocean.assets.resolve(ALG_did)
+        print("Waiting for algorithm DDO")
+        pass
+    compute_service = DATA_DDO.get_service('compute')
+    algo_service = ALG_DDO.get_service('access')
+    print(f'Algorithm DDO is {ALG_DDO}')
+
+    trusted_algorithms.add_publisher_trusted_algorithm(DATA_DDO, ALG_DDO.did, 'https://aquarius.oceanprotocol.com')
+    ocean.assets.update(DATA_DDO, publisher_wallet=wallet)
+
+
+    # Datatoken buying
+    data_token_address = f'0x{DATA_did[7:]}'
+    data_token = ocean.get_data_token(data_token_address)
+    if data_token.balanceOf(wallet.address) < to_wei(1):
+        print('Not enough datatokens in wallet, buying...')
+        buy_dt(config, wallet, ocean, OCEAN_token, DATA_did)
+
+    algo_token_address = f'0x{ALG_did[7:]}'
+    algo_token = ocean.get_data_token(algo_token_address)
+    print(f"You have {pretty_ether_and_wei(algo_token.balanceOf(wallet.address), algo_token.symbol())} algorithm tokens.")
+
+    result = run_c2d(ocean, wallet, DATA_did, ALG_did, ALG_datatoken, compute_service, algo_service)
+    return result
+
+def configure_ocean(config):
     conf = {
     'network' : 'https://rinkeby.infura.io/v3/d163c48816434b0bbb3ac3925d6c6c80' if config.runtime_options.get("network") is None else config.runtime_options.get("network"),
     'BLOCK_CONFIRMATIONS': 0,
@@ -33,7 +71,9 @@ def deploy(base_path: Path,
     OCEAN_token = BToken(ocean.web3, ocean.OCEAN_address)
     provider_url = DataServiceProvider.get_url(ocean.config)
 
+    return ocean, OCEAN_token, provider_url
 
+def algo_publish(config, wallet, ocean, provider_url):
     """
     Algorithm publishing
 
@@ -44,11 +84,6 @@ def deploy(base_path: Path,
     - dataset we want to train on specified
     - model metadata (name, date, compute, etc.)
     """
-
-    wallet = Wallet(ocean.web3, config.runtime_options.get("wallet_private_key"), transaction_timeout=20, block_confirmations=0)
-    print(f"wallet.address = '{wallet.address}'")
-    assert wallet.web3.eth.get_balance(wallet.address) > 0, "need ETH"
-
 
     # Publish ALG datatoken
     ALG_datatoken = ocean.create_data_token('ALG1', 'ALG1', wallet, blob=ocean.config.metadata_cache_uri)
@@ -104,20 +139,9 @@ def deploy(base_path: Path,
     services=[ALG_access_service],
     data_token_address=ALG_datatoken.address)
 
-    DATA_did = config.runtime_options.get("dt_did")
-    ALG_did = ALG_ddo.did
-    DATA_DDO = ocean.assets.resolve(DATA_did)  # make sure we operate on the updated and indexed metadata_cache_uri versions
-    ALG_DDO = ocean.assets.resolve(ALG_did)
-    while ALG_DDO == None:
-        ALG_DDO = ocean.assets.resolve(ALG_did)
-        print("Waiting for algorithm DDO")
-        pass
-    compute_service = DATA_DDO.get_service('compute')
-    algo_service = ALG_DDO.get_service('access')
-    print(f'Algorithm DDO is {ALG_DDO}')
+    return ALG_ddo, ALG_datatoken
 
-    trusted_algorithms.add_publisher_trusted_algorithm(DATA_DDO, ALG_DDO.did, 'https://aquarius.oceanprotocol.com')
-    ocean.assets.update(DATA_DDO, publisher_wallet=wallet)
+def buy_dt(config, wallet, ocean, OCEAN_token, did):
     """
     Datatoken buying
 
@@ -125,30 +149,22 @@ def deploy(base_path: Path,
     - wallet from previous step
     - datatoken DID and pool address
     """
-
     pool_address = config.runtime_options.get("dt_pool")
-
-    assert wallet is not None, "Wallet error, initialize app again"
+    assert wallet is not None, "Wallet error, run pipeline again"
     # Get asset, datatoken_address
-    data_token_address = f'0x{DATA_did[7:]}'
-    algo_token_address = f'0x{ALG_did[7:]}'
+    asset = ocean.assets.resolve(did)
+    data_token_address = f'0x{did[7:]}'
 
     print('Executing Transaction')
-
-    # Wallet status
+    #my wallet
     print(f"Environment Wallet Address = '{wallet.address}'")
     print(f"Wallet OCEAN = {pretty_ether_and_wei(OCEAN_token.balanceOf(wallet.address))}")
     print(f"Wallet ETH = {pretty_ether_and_wei(ocean.web3.eth.get_balance(wallet.address))}")
-
-    # Verify wallet has ETH
+    #Verify that wallet has ETH
     assert ocean.web3.eth.get_balance(wallet.address) > 0, "need test ETH"
-    #Verify wallet has OCEAN
+    #Verify that wallet has OCEAN
     assert OCEAN_token.balanceOf(wallet.address) > 0, "need test OCEAN"
-    #Buy 1.0 datatoken - the amount needed to consume the dataset.
     data_token = ocean.get_data_token(data_token_address)
-    algo_token = ocean.get_data_token(algo_token_address)
-    print(f"You have {pretty_ether_and_wei(algo_token.balanceOf(wallet.address), algo_token.symbol())} algorithm tokens.")
-
     print('Buying Data Token')
     ocean.pool.buy_data_tokens(
         pool_address,
@@ -158,11 +174,10 @@ def deploy(base_path: Path,
     )
     print(f"You have {pretty_ether_and_wei(data_token.balanceOf(wallet.address), data_token.symbol())}.")
 
-
+def run_c2d(ocean, wallet, DATA_did, ALG_did, ALG_datatoken, compute_service, algo_service):
     """
     Running C2D
     """
-
     # order & pay for dataset
     dataset_order_requirements = ocean.assets.order(
         DATA_did, wallet.address, service_type=compute_service.type
