2a0b9701a7692f37f23ff301653267e6ae8ab008
Jakub Smékal <jakub.smekal@gmail.com>
2022-06-30 20:38:31 +0100

    WIP: added algo-pushed requirement

--- sameproject/ops/ocean/render.py
+++ sameproject/ops/ocean/render.py
@@ -16,18 +16,19 @@ ocean_step_template = "step.jinja"
 
 def render(compile_path: str, steps: list, same_config: dict) -> Tuple[Path, str]:
     """Renders the notebook into a root file and a series of step files according to the target requirements. Returns an absolute path to the root file for deployment."""
-    templateDir = os.path.dirname(os.path.abspath(__file__))
-    templateLoader = FileSystemLoader(templateDir)
-    print(f"Template dir {templateDir}")
-    env = Environment(trim_blocks=True, loader=templateLoader)
+    if same_config.runtime_options.get("algo_pushed") == "False":
+        templateDir = os.path.dirname(os.path.abspath(__file__))
+        templateLoader = FileSystemLoader(templateDir)
+        print(f"Template dir {templateDir}")
+        env = Environment(trim_blocks=True, loader=templateLoader)
 
-    root_file_string = _build_step_file(env, next(iter(steps.values())), same_config)
-    root_pipeline_name = f"root_pipeline_{uuid4().hex.lower()}"
-    root_path = Path(compile_path) / f"{root_pipeline_name}.py"
-    helpers.write_file(root_path, root_file_string)
+        root_file_string = _build_step_file(env, next(iter(steps.values())), same_config)
+        root_pipeline_name = f"root_pipeline_{uuid4().hex.lower()}"
+        root_path = Path(compile_path) / f"{root_pipeline_name}.py"
+        helpers.write_file(root_path, root_file_string)
 
-    return (compile_path, root_file_string) # note: root_file_string replaced root_pipeline_name
-
+        return (compile_path, root_file_string) # note: root_file_string replaced root_pipeline_name
+    print("Model already rendered and published.")
 
 def _build_step_file(env: Environment, step: Step, same_config) -> str:
     with open(sameproject.ops.explode.__file__, "r") as f:
