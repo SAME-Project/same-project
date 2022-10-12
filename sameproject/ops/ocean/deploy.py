375aacc67fa480084a81b4fd36465d882aa97ce4
Jakub Smékal <jakub.smekal@gmail.com>
2022-10-12 09:51:25 +0100

    WIP: refactoring ocean to aws

--- sameproject/ops/ocean/deploy.py
+++ sameproject/ops/ocean/deploy.py
@@ -2,9 +2,15 @@ from sameproject.data.config import SameConfig
 from sameproject.ops import helpers
 from pathlib import Path
 import importlib
-import os
+import kfp
 
-def deploy(base_path: Path,
-           root_file: str, # root function with notebook code (string)
-           config: SameConfig):
-    return
+
+def deploy(base_path: Path, root_file: str, config: SameConfig):
+    with helpers.add_path(str(base_path)):
+        root_module = importlib.import_module(root_file)  # python module
+
+        kfp_client = kfp.Client()  # only supporting 'kubeflow' namespace
+        return kfp_client.create_run_from_pipeline_func(
+            root_module.root,
+            arguments={},
+        )
