102f6ebc9b52127b00988390d4976c9dc2caeecc
Jakub Smékal <jakub.smekal@gmail.com>
2022-10-12 14:38:17 +0100

    WIP: refactoring to boto3

--- sameproject/ops/ocean/deploy.py
+++ sameproject/ops/ocean/deploy.py
@@ -2,15 +2,15 @@ from sameproject.data.config import SameConfig
 from sameproject.ops import helpers
 from pathlib import Path
 import importlib
-import kfp
+import boto3
 
 
 def deploy(base_path: Path, root_file: str, config: SameConfig):
     with helpers.add_path(str(base_path)):
         root_module = importlib.import_module(root_file)  # python module
 
-        kfp_client = kfp.Client()  # only supporting 'kubeflow' namespace
-        return kfp_client.create_run_from_pipeline_func(
+        client = boto3.client('ec2')
+        return client.create_run_from_pipeline_func(
             root_module.root,
             arguments={},
         )
