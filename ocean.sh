40799268c81881cc8354cdd847d8435b72a3f44b
Jakub Smékal <jakub.smekal@gmail.com>
2022-11-07 13:45:08 +0100

    Update ocean.sh

--- ocean.sh
+++ ocean.sh
@@ -2,10 +2,6 @@
 
 cd ../data/transformations/
 
-cli_execute_transform_command  algorithm converted_script.py
-
-python3.8 converted_script.py
-
 same init
 
 same run -t ocean --no-deploy --persist-temp-files
