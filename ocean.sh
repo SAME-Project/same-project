33b85b8e9c334c0e0a258dcd4d8fb77ac0e6745f
Jakub Smékal <jakub.smekal@gmail.com>
2022-11-08 08:51:24 +0000

    WIP: updated dockerfile & bash script with nbconvert

--- ocean.sh
+++ ocean.sh
@@ -2,14 +2,8 @@
 
 cd /data/transformations/
 
-<<<<<<< HEAD
 mv 0 hello.ipynb
 
 jupyter nbconvert hello.ipynb --to python
 
 python3.8 hello.py
-=======
-same init
-
-same run -t ocean --no-deploy --persist-temp-files
->>>>>>> 40799268c81881cc8354cdd847d8435b72a3f44b
