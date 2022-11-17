dee1ae760b361dfef48789e492f1903fb285e9e4
Jakub Smékal <jakub.smekal@gmail.com>
2022-11-17 11:05:18 +0000

    WIP: ocean.sh for 0.4

--- ocean.sh
+++ ocean.sh
@@ -6,6 +6,10 @@ mv algorithm hello.ipynb
 
 same init
 
+export KF_PIPELINES_ENDPOINT_ENV='ml_pipeline.kubeflow.svc.cluster.local'
+
+echo KF_PIPELINES_ENDPOINT_ENV
+
 same run
 
 jupyter nbconvert hello.ipynb --to python
