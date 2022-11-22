4042ab974dda25077630f49ea98a0337b6afd236
Jakub Smékal <jakub.smekal@gmail.com>
2022-11-22 08:10:38 +0000

    WIP: hardcoding host url

--- sameproject/ops/kubeflow/deploy.py
+++ sameproject/ops/kubeflow/deploy.py
@@ -9,7 +9,9 @@ def deploy(base_path: Path, root_file: str, config: SameConfig):
     with helpers.add_path(str(base_path)):
         root_module = importlib.import_module(root_file)  # python module
 
-        kfp_client = kfp.Client()  # only supporting 'kubeflow' namespace
+        print("getting kfp_client")
+        kfp_client = kfp.Client(host="ml_pipeline.kubeflow.svc.cluster.local:8888")  # only supporting 'kubeflow' namespace
+        print("got kfp_client")
         return kfp_client.create_run_from_pipeline_func(
             root_module.root,
             arguments={},
